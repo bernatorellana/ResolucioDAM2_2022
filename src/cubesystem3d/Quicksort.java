efb1397f77fc84617a60de5f4cf80d0713b1af64
bernat <bernat.orellana@gmail.com>
2022-02-07 19:02:56 +0100

    Me lhan fotut

--- src/cubesystem3d/Quicksort.java
+++ src/cubesystem3d/Quicksort.java
@@ -6,7 +6,7 @@ public class Quicksort  {
     private int[] numbers;
     private int number;
 
-    public void sort(int[] values) {
+    public void sortNow(int[] values) {
         // check for empty or null array
         if (values ==null || values.length==0){
             return;
